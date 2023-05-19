8355f10fc81a65882ce7f8d95869c5fe0b9c7930
abdessalam staili <abdoycode@gmail.com>
2023-05-19 19:25:49 +0100

    feat : finish backend with laravel V10

--- calculator-Apis/app/Http/Controllers/CalculatorController.php
+++ calculator-Apis/app/Http/Controllers/CalculatorController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class CalculatorController extends Controller
 {
-    public function calculator(Request $request) {
+    public function calculator(Request $request) 
+    {
         $current = $request->current;
         
         $result = eval("return $current;"); 
@@ -16,34 +17,29 @@ class CalculatorController extends Controller
 
      public function EquationA(Request $request)
     {
-
         $valueA = $request->valueA;
         $valueB= $request->valueB;
         if($valueB==0){
-        return response()->json(0);
-
+            return response()->json(0);
         }else{
             $result = $valueB/$valueA;
             return response()->json($result);
         }
-      
     }
+
     public function EquationB(Request $request)
     {
-
         $valueA = $request->valueA;
         $valueB= $request->valueB;
         $valueC= $request->valueC;
 
         $Delta = (pow($valueB,2))-(4*$valueA*$valueC);
 
-       
         if($Delta<0){
             return response()->json("n'a pas de solution réelle.");
         }elseif ($Delta==0){
             $x = -1*$valueB/2*$valueA;
-                return response()->json($x);
-               
+             return response()->json($x);   
         }else {
             $x1= number_format(((-1*$valueB) - sqrt($Delta))/2*$valueA,2);
             $x2= number_format(((-1*$valueB) + sqrt($Delta))/2*$valueA,2);
@@ -51,6 +47,5 @@ class CalculatorController extends Controller
             return response()->json($x);
         }
         
-
     }
 }
